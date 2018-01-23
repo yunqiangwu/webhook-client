7f5c727df503dd9a8406d5963e5800d104c83f8d
吴云强 <yunqiang.wu@hand-china.com>
2018-01-24 01:59:16 +0800

    exec

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -14,9 +14,8 @@ restartBtn.onclick = ()=>{
 		showMsg(JSON.stringify(error,null,2));
 		restartBtn.disabled = false;
 	});
-
 }
 
 function showMsg(msg) {
-	restartResult.innerHTML= `<pre>${msg}\n\n</pre>`
+	restartResult.innerHTML += `<pre>${msg}\n\n</pre>`
 }
